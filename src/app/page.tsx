6e9148daf2f1a6ead39d90bb4f210e690c67f5b4
Anish <anishtanvar@gmail.com>
2024-12-13 00:18:34 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,59 @@
+"use client";
+import { useState } from "react";
+import arrowe from "../../public/arrow.png";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const images = [
+  { id: 1, src: "/images/strength.jpg", title: "STRENGTH" },
+  { id: 2, src: "/images/mobility.jpg", title: "MOBILITY" },
+  { id: 3, src: "/images/drills.jpg", title: "DRILLS" },
+];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+export default function Home() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleClick = (index: number) => {
+   
+    if(index === 3){
+      index = 0
+    }
+    console.log("Index-", index)
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="mx-3 md:mx-10 lg:mx-40 w-full rounded-[2rem] flex justify-center items-center h-96 overflow-hidden">
+        {images.map((image, index) => (
+          <div
+            key={image.id}
+            className={`duration-500 relative group ${
+              index === activeIndex 
+              ? "flex-grow h-full" 
+              : "flex-none w-20 md:w-40 lg:w-72 h-full"
+            } cursor-pointer overflow-hidden`}
+            onClick={() => handleClick(index + 1)}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            <img
+              src={image.src}
+              alt={image.title}
+              className="w-full h-full object-cover"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <div className="absolute top-4 left-4 text-white font-semibold text-base md:text-xl flex w-full justify-between">
+              {image.title}
+              {/* Arrow Button - Initially Hidden */}
+              {index === activeIndex && (
+              <div className="mr-8 hidden group-hover:flex items-center justify-center">
+                <Image
+                  src={arrowe}
+                  alt="arrowe"
+                  className="bg-white w-8 h-8 rounded-full"
+                />
+              </div>)}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
